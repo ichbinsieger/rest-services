ebfbe4ba1cb17367021f4f1505c9a77f83e49c74
IAmMasterCraft <bolu.akinsefunmi@gmail.com>
2021-07-17 01:22:02 +0100

    footer on the way

---
+++ components/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <div class="columns">
+            <div class="column">
+                REST SERVICES <br />
+                Something about rest services
+            </div>
+            <div class="column">
+                <MailIcon /> admin@restservices.org <br />
+                <PhoneIcon /> +44 1234 567890 <br />
+                <LocationMarkerIcon /> 1, rest service avenue, rest city
+            </div>
+        </div>
+        <div class="columns has-text-centered">
+            All Rights Reserved <br />
+            REST SERVICES <br />
+            &copy; {{ year }}.
+        </div>
+    </div>
+</template>
+
+<script>
+    import { MailIcon, PhoneIcon, LocationMarkerIcon } from "@vue-hero-icons/outline"
+
+    export default {
+        name: "Footer",
+        components: {
+            MailIcon,
+            PhoneIcon,
+            LocationMarkerIcon,
+        },
+        data() {
+            return {
+                year: new Date().getFullYear(),
+            }
+        },
+    }
+</script>
