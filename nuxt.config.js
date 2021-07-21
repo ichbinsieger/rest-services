0b03c1a7f313fe8183fbd4fb7607c51c42433b68
IAmMasterCraft <bolu.akinsefunmi@gmail.com>
2021-07-21 15:25:28 +0100

    ionicons added

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,12 +15,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '@/assets/css/mystyles.css',
-  ],
+  css: ['@/assets/css/mystyles.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['@plugins/ionicons'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
