dc523d62cc4c91aa192ff902baba02f236befe5b
IAmMasterCraft <bolu.akinsefunmi@gmail.com>
2021-07-21 21:22:29 +0100

    working with boss seiger

---
+++ components/seiger/Contact.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1 class="bg-green-500 mt-6 pt-6">Hello world I am contact</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Contact",
+    }
+</script>
---
+++ pages/contact.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="">
+    <Contact />
+  </div>
+</template>
+
+<script lang="js">
+
+import Contact from '~/components/seiger/Contact'
+
+export default {
+  name: 'App',
+  components: {
+    Contact,
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
