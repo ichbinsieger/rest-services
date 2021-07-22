612a5925b9d92eecb8473c9696a27c4b4738fc8e
IAmMasterCraft <bolu.akinsefunmi@gmail.com>
2021-07-22 01:22:53 +0100

    services

--- components/Services.vue
+++ components/Services.vue
@@ -1,33 +1,71 @@
 <template>
-    <div class="">
-        <h2>Our services</h2>
-        <p>What we do</p>
-        <div class="columns">
+    <div class="mx-6 px-6">
+        <div class="has-text-black has-text-centered">
+            <h2 class="has-text-weight-bold is-size-3">Our Services</h2>
+            <p>What we do</p>
+        </div>
+        <div class="columns py-6">
             <div class="column mx-2">
                 <div class="card">
-                    <div class="is-rounded">
-                        <figure class="image">
-                            <img src="http://themepure.net/template/sycho-prv/sycho/assets/img/services/services-1.jpg" />
-                        </figure>
-                        <div class="buttons">
-                            <b-button type="is-primary" expanded>Expanded</b-button>
+                    <div class="card-content">
+                        <div class="has-text-centered has-text-primary py-3">
+                            <ion-icon name="thumbs-up-sharp"></ion-icon>
                         </div>
+                        <h4 class="has-text-primary is-size-4 has-text-weight-semibold">Support</h4>
+                        <p>We create a strategy which will encourage residents to take control of their own lives by developing the skills necessary for independent living.</p>
                     </div>
                 </div>
             </div>
 
             <div class="column mx-2">
                 <div class="card">
-                    <div class="is-rounded">
-                        <figure class="image">
-                            <img src="http://themepure.net/template/sycho-prv/sycho/assets/img/services/services-1.jpg" />
-                        </figure>
-                        <div class="buttons">
-                            <b-button type="is-primary" expanded>Expanded</b-button>
+                    <div class="card-content">
+                        <div class="has-text-centered has-text-primary py-3">
+                            <ion-icon name="home-sharp"></ion-icon>
                         </div>
+                        <h4 class="has-text-primary is-size-4 has-text-weight-semibold">Accomodation</h4>
+                        <p>Accomodation is provided with intensive housing management to meet the needs of all our residents, including shared and single units.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="columns py-6">
+            <div class="column mx-2">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="has-text-centered has-text-primary py-3">
+                            <ion-icon name="home-sharp"></ion-icon>
+                        </div>
+                        <h4 class="has-text-primary is-size-4 has-text-weight-semibold">Volunteers</h4>
+                        <p>Our dedicated volunteers work alongside our support workers and housing officers to provide additional support in the implementation of the individualised strategy to enable and encourage personal growth and independence.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="column mx-2">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="has-text-centered has-text-primary py-3">
+                            <ion-icon name="chatbubbles-sharp"></ion-icon>
+                        </div>
+                        <h4 class="has-text-primary is-size-4 has-text-weight-semibold">Referrals</h4>
+                        <p>We work alongside specialist services, local authorities and third sector organisations to ensure that those who meet our criteria have the best possible strategy and support to meet their individual needs.</p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+    
+    export default {
+        name: "Services",
+    }
+</script>
+
+<style scoped>
+    ion-icon {
+        font-size: 64px;
+    }
+</style>
